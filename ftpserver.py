d8ce20a56b72b9361b2cee1532c2011bacca71ee
chaitanya <kvchaitanyavarma@gmail.com>
2018-12-11 19:03:24 +0530

    Comment Tweaks

--- ftpserver.py
+++ ftpserver.py
@@ -3,6 +3,7 @@ from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
 authorizer = DummyAuthorizer()
+#authorizer.add_user("Your User ID", "Your Password", "Location for Storage", perm="elradfmw")
 authorizer.add_user("user1", "rudra@kiranm", "F:\Share", perm="elradfmw")
 authorizer.add_user("user2", "rudra@suryad", "F:\Share", perm="elradfmw")
 authorizer.add_user("user3", "rudra@saim", "F:\Share", perm="elradfmw")
@@ -11,9 +12,7 @@ authorizer.add_user("user5", "rudra@phanip", "F:\Share", perm="elradfmw")
 authorizer.add_user("user6", "rudra@yamunak", "F:\Share", perm="elradfmw")
 authorizer.add_user("user7", "rudra@chaituk", "F:\Share", perm="elradfmw")
 authorizer.add_user("user8", "rudra@chaitukv", "F:\BackUp", perm="elradfmw")
-#authorizer.add_user("user", "123456", "F:\Share", perm="elradfmw")
-#authorizer.add_user("user9", "anonymous1", "F:\test2", perm="elradfmw")
-#authorizer.add_anonymous("/home/pi")
+
 
 handler = FTPHandler
 handler.authorizer = authorizer
